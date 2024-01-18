4811edb7aef04fddabee57287571f71ab984c266
Jin0331 <sempre813@naver.com>
2024-01-18 21:46:58 +0900

    [Design] Main TableView 및 Cell 나타나도록

--- Recap Assignment/Resource/Extension.swift
+++ Recap Assignment/Resource/Extension.swift
@@ -5,4 +5,16 @@
 //  Created by JinwooLee on 1/18/24.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController : ResuableProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UITableViewCell : ResuableProtocol {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
