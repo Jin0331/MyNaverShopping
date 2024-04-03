f420367f6c31bc84d5a98599a279db370577f5f4
Jin0331 <sempre813@naver.com>
2024-04-04 01:47:04 +0900

    [✅Refactor] 메인 화면 RxSwift + MVVM 적용

--- SeSAC Shopping/Presentation/ViewModel/MainViewModel.swift
+++ SeSAC Shopping/Presentation/ViewModel/MainViewModel.swift
@@ -6,21 +6,86 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class MainViewModel {
     
-    var inputSearchKeywordList = Observable(UserDefaultManager.shared.search)
+    let disposeBag = DisposeBag()
+    let input = Input()
+    let output = Output()
     
-    var inputSearchKeywordListCount : Int {
-        return inputSearchKeywordList.value.count
+    struct Input {
+        let inputSearchKeywordList = BehaviorSubject(value: UserDefaultManager.shared.search)
+        let inputSearchKeyword = PublishSubject<String>()
+        let inputSearchTransition = PublishSubject<Void>()
+        let inputRemoveButtonTap = PublishSubject<Void>()
+        let inputTableRemoveButtonTap = PublishRelay<Int>()
     }
     
-    var inputSearchKeywordListBool : Bool {
-        return inputSearchKeywordListCount == 0 ? true : false
+    struct Output {
+        let outputSearchKeywordList = BehaviorRelay<[String]>(value: [])
+        let outputSearchKeywordListIsExist = BehaviorRelay(value: true)
+
+    }
+    
+    init() {
+        transform()
     }
     
     
-    func cellForRowAt(_ indexPath : IndexPath) -> String {
-        return self.inputSearchKeywordList.value[indexPath.row]
+    private func transform() {
+        
+        input.inputSearchKeywordList
+            .bind(with: self) { owner, value in
+                
+                print(value)
+                if value.isEmpty {
+                    owner.output.outputSearchKeywordListIsExist.accept(true)
+                } else {
+                    owner.output.outputSearchKeywordListIsExist.accept(false)
+                }
+                
+                owner.output.outputSearchKeywordList.accept(value)
+                
+
+            }
+            .disposed(by: disposeBag)
+        
+        input.inputSearchKeyword
+            .bind(with: self) { owner, value in
+                // 기존 검색이 있었는지 판단하고, 과거의 값을 지우고 추가
+                
+                var editList = UserDefaultManager.shared.search
+                
+                if editList.contains(value){
+                    editList.remove(at: editList.firstIndex(of: value)!)
+                }
+                editList.insert(value, at: 0) // 새로운 값은 무조건 앞으로
+                
+                // UserDefault Update
+                UserDefaultManager.shared.search = editList
+                owner.input.inputSearchKeywordList.onNext(UserDefaultManager.shared.search)
+            }
+            .disposed(by: disposeBag)
+        
+        input.inputRemoveButtonTap
+            .bind(with: self) { owner, _ in
+                UserDefaultManager.shared.search = []
+                owner.input.inputSearchKeywordList.onNext(UserDefaultManager.shared.search)
+            }
+            .disposed(by: disposeBag)
+        
+        input.inputTableRemoveButtonTap
+            .bind(with: self) { owner, value in
+                var editList = UserDefaultManager.shared.search
+                editList.remove(at: value)
+                owner.input.inputSearchKeywordList.onNext(editList)
+            }
+            .disposed(by: disposeBag)
+        
     }
+    
+    
+    
 }
